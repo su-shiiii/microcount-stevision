c0a6038bdc954f38b85ebe573a5aa151c3c4128e
su-shiiii <shilohsison27@gmail.com>
2026-08-07 07:10:18 +0000

    Update website design and features

--- sample.js
+++ sample.js
@@ -10,34 +10,31 @@ continueBtn.addEventListener("click", function () {
     const school = document.getElementById("schoolName").value.trim();
     const section = document.getElementById("section").value.trim();
     const source = document.getElementById("source").value;
+    const waterCondition = document.getElementById("waterCondition").value;
     const numImages = document.getElementById("numImages").value;
     const microscope = document.getElementById("microscopeType").value;
-    if (
-    school === "" ||
-    section === "" ||
-    source === "" ||
-    numImages === "" ||
-    microscope === ""
-) {
-    alert("Please complete all fields.");
-    return;
-}
 
+    // Check if all fields are completed
     if (
         school === "" ||
         section === "" ||
         source === "" ||
-        numImages === ""
+        waterCondition === "" ||
+        numImages === "" ||
+        microscope === ""
     ) {
         alert("Please complete all fields.");
         return;
     }
 
+    // Save information
     localStorage.setItem("schoolName", school);
     localStorage.setItem("section", section);
     localStorage.setItem("source", source);
+    localStorage.setItem("waterCondition", waterCondition);
     localStorage.setItem("numImages", numImages);
+    localStorage.setItem("microscopeType", microscope);
 
+    // Go to upload page
     window.location.href = "upload.html";
-
 });
